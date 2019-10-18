a48e33c870d2f182def529af34a8e92bbbe5991a
Don-Droid <raldon.baxter@mymona.uwi.edu>
2019-10-17 22:24:05 -0500

    Exercise 4 Completed - Winner checked and status updated with congratulation message

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 let clickCounter = 1;
+let turn = '';
 
 function applyAttributes(){
     let boxes = document.getElementById("board").children;    
@@ -20,13 +21,24 @@ function click(e){
        if (clickCounter % 2 === 0){
             document.getElementById(clickedItem).innerText = "O";
             document.getElementById(clickedItem).className = "square O";
+            //checkForWinner();
+            turn = "X";
+            let played = "O";
             clickCounter += 1;
+            checkForWinner(played);
+			//alert (turn);
        }else{
             document.getElementById(clickedItem).innerText = "X";
             document.getElementById(clickedItem).className = "square X";
+           // checkForWinner();
+            turn = "O";
+            let played = "X";
             clickCounter +=1 ;
-       }         
-    }       
+            checkForWinner(played);
+			//alert (turn);
+       } 
+    }
+           
 }
 
 function mHover(e){
@@ -42,3 +54,30 @@ function mLeave (e){
         document.getElementById(leftItem).classList.remove("hover");
     }
 }
+
+function checkForWinner(letter){
+    let result = false;
+    if (check(1, 2, 3, letter)||
+        check(4, 5, 6, letter)||
+        check(7, 8, 9, letter)||
+        check(1, 5, 9, letter)||
+        check(3, 5, 7, letter)||
+        check(1, 4, 7, letter)||
+        check(2, 5, 8, letter)||
+        check(3, 6, 9, letter)){
+            result = true;
+        }
+}
+
+function check(a, b, c, letter){
+    let result = false;
+    if(getBox (a) == letter && getBox (b) == letter && getBox (c) == letter){
+        result = true;
+        document.getElementById("status").innerText = "Congratulations! " + letter + " is the Winner!";
+        document.getElementById("status").classList.add("you-won");
+    }
+}
+
+function getBox(number){
+    return document.getElementById(""+number).innerText;
+} 
